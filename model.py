93d55d9f192fe43e42095dda1e9c81f811907b55
dcDun <12344307+dcDun@users.noreply.github.com>
2017-11-25 00:43:48 -0600

    added preprocessing of image and applyed leNet to model

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 lines = []
-with open('../data/driving_log.csv') as csvfile:
+with open('../Data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -13,7 +13,7 @@ measurements = []
 for lines in lines:
     source_path = lines[0]
     filename = source_path.split('/')[-1]
-    current_path = '../data/IMG/' + filename
+    current_path = '../Data/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
@@ -23,13 +23,24 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
+exit()
