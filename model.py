c910de2d85a985559e4c05540d17dcd8d6d41e31
dcDun <12344307+dcDun@users.noreply.github.com>
2017-11-25 23:52:56 -0600

    added image cropping and updated to keras api 2 syntax

--- model.py
+++ model.py
@@ -27,15 +27,16 @@ y_train = np.array(measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Conv2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
 
-model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(Conv2D(6, (5, 5),activation="relu"))
 model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(Conv2D(6, (5, 5),activation="relu"))
 model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(120))
@@ -43,7 +44,7 @@ model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=1)
 
 model.save('model.h5')
 exit()
