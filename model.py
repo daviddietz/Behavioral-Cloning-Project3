fe8e668adc59bb4f546272cf956635ea84e42012
dcDun <12344307+dcDun@users.noreply.github.com>
2017-11-26 23:45:58 -0600

    fixed output size in final layer

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 lines = []
-with open('../Data/driving_log.csv') as csvfile:
+with open('../Data/archivedData/dataSet2/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -13,7 +13,7 @@ measurements = []
 for lines in lines:
     source_path = lines[0]
     filename = source_path.split('/')[-1]
-    current_path = '../Data/IMG/' + filename
+    current_path = '../Data/archivedData/dataSet2/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
     images.append(np.fliplr(image))
@@ -33,18 +33,19 @@ model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 
-model.add(Conv2D(24, (5, 5), subsample=(2, 2), activation="relu"))
-model.add(Conv2D(36, (5, 5), subsample=(2, 2), activation="relu"))
-model.add(Conv2D(48, (5, 5), subsample=(2, 2), activation="relu"))
-model.add(Conv2D(64, (3, 3), subsample=(2, 2), activation="relu"))
-model.add(Conv2D(64, (3, 3), subsample=(2, 2), activation="relu"))
+model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu"))
+model.add(Conv2D(36, (5, 5), strides=(2, 2), activation="relu"))
+model.add(Conv2D(48, (5, 5), strides=(2, 2), activation="relu"))
+model.add(Conv2D(64, (3, 3), activation="relu"))
+model.add(Conv2D(64, (3, 3), activation="relu"))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
+model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=2)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=1)
 
 model.save('model.h5')
 exit()
