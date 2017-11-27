2a26fe49a1605a489b2736d5dcc2e265801a221d
dcDun <12344307+dcDun@users.noreply.github.com>
2017-11-27 01:35:44 -0600

    fixed bug

--- model.py
+++ model.py
@@ -3,24 +3,26 @@ import cv2
 import numpy as np
 
 lines = []
-with open('../Data/driving_log.csv') as csvfile:
+with open('../Data/archivedData/dataSet2/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
 
 images = []
 measurements = []
-for lines in lines:
-    source_path = lines[0]
-    filename = source_path.split('/')[-1]
-    current_path = '../Data/IMG/' + filename
-    image = cv2.imread(current_path)
-    images.append(image)
-    images.append(np.fliplr(image))
+for line in lines:
+    for i in range(3):
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        current_path = '../Data/archivedData/dataSet2/IMG/' + filename
+        image = cv2.imread(current_path)
+        measurement = float(line[3])
 
-    measurement = float(line[3])
-    measurements.append(measurement)
-    measurements.append(measurement*-1.0)
+        images.append(image)
+        measurements.append(measurement)
+
+        images.append(np.fliplr(image))
+        measurements.append(-measurement)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -31,7 +33,7 @@ from keras.layers.convolutional import Conv2D, Cropping2D
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
-#model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 
 model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu"))
 model.add(Conv2D(36, (5, 5), strides=(2, 2), activation="relu"))
@@ -45,7 +47,7 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=1)
 
 model.save('model.h5')
 exit()
