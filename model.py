feaca1791a2d8c77b750e7d258555f29c49bd109
dcDun <12344307+dcDun@users.noreply.github.com>
2017-11-26 22:53:42 -0600

    implement new nn

--- model.py
+++ model.py
@@ -28,23 +28,23 @@ y_train = np.array(measurements)
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers.convolutional import Conv2D, Cropping2D
-from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 
-model.add(Conv2D(6, (5, 5),activation="relu"))
-model.add(MaxPooling2D())
-model.add(Conv2D(6, (5, 5),activation="relu"))
-model.add(MaxPooling2D())
+model.add(Conv2D(24, (5, 5), subsample=(2, 2), activation="relu"))
+model.add(Conv2D(36, (5, 5), subsample=(2, 2), activation="relu"))
+model.add(Conv2D(48, (5, 5), subsample=(2, 2), activation="relu"))
+model.add(Conv2D(64, (3, 3), subsample=(2, 2), activation="relu"))
+model.add(Conv2D(64, (3, 3), subsample=(2, 2), activation="relu"))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
-model.add(Dense(1))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=2)
 
 model.save('model.h5')
 exit()
