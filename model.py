506ccdd976a66834bf182fae51329c53e3abf300
dcDun <12344307+dcDun@users.noreply.github.com>
2017-11-28 23:32:02 -0600

    added loss visualization

--- model.py
+++ model.py
@@ -1,6 +1,10 @@
 import csv
 import cv2
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Conv2D, Cropping2D
+import matplotlib.pyplot as plt
 
 lines = []
 with open('../Data/driving_log.csv') as csvfile:
@@ -13,7 +17,7 @@ measurements = []
 for line in lines:
     for i in range(3):
         measurement = float(line[3])
-        correction = 0.2
+        correction = 0.25
         if i == 1:
             # Apply correction to left image
             measurement = measurement + correction
@@ -34,10 +38,6 @@ for line in lines:
 X_train = np.array(images)
 y_train = np.array(measurements)
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
-from keras.layers.convolutional import Conv2D, Cropping2D
-
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((32, 20), (0, 0))))
@@ -54,7 +54,19 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+history_object = model.fit_generator(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5, verbose=1)
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
 
 model.save('model.h5')
 exit()
