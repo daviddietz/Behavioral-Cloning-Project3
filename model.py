8691b3dde698bcd048eb70dafe1aa2ed92174193
daviddietz <12344307+dcDun@users.noreply.github.com>
2018-01-03 06:38:44 -0600

    updated to keras2. Final model for submission

--- model.py
+++ model.py
@@ -2,12 +2,11 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Dropout, MaxPooling2D
+from keras.layers import Flatten, Dense, Lambda, Dropout
 from keras.layers.convolutional import Conv2D, Cropping2D
 # import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
-from random import randint
 
 sample_images = []
 with open('../Data/driving_log.csv') as csvfile:
@@ -16,7 +15,7 @@ with open('../Data/driving_log.csv') as csvfile:
         sample_images.append(line)
 
 train_image_samples, validation_image_samples = train_test_split(sample_images, test_size=0.2)
-BATCH_SIZE = 10
+BATCH_SIZE = 128
 
 
 def generator(samples, batch_size=BATCH_SIZE):
@@ -30,20 +29,16 @@ def generator(samples, batch_size=BATCH_SIZE):
             measurements = []
 
             for batch_sample in batch_samples:
-                for image in range(3):
+                for imageCameraAngle in range(3):
                     measurement = float(batch_sample[3])
-                    # if measurement == 0.00:
-                    #     keep_prob = randint(0, 3)
-                    #     if keep_prob == 1:
-                    #         continue
                     correction = 0.25
-                    if image == 1:
+                    if imageCameraAngle == 1:
                         # Apply correction to left image
                         measurement = measurement + correction
-                    if image == 2:
+                    if imageCameraAngle == 2:
                         # Apply correction to right image
                         measurement = measurement - correction
-                    source_path = line[image]
+                    source_path = batch_sample[imageCameraAngle]
                     filename = source_path.split('/')[-1]
                     current_path = '../Data/IMG/' + filename
                     image = cv2.imread(current_path)
@@ -69,21 +64,20 @@ model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu"))
 model.add(Conv2D(36, (5, 5), strides=(2, 2), activation="relu"))
 model.add(Conv2D(48, (3, 3), strides=(2, 2), activation="relu"))
-#model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
 model.add(Conv2D(64, (3, 3), activation="relu"))
 model.add(Conv2D(64, (3, 3), activation="relu"))
 model.add(Flatten())
 model.add(Dropout(0.5))
-model.add(Dense(units=100, activation="relu"))
+model.add(Dense(units=100))
 model.add(Dense(units=50))
-#model.add(Dropout(0.5))
+model.add(Dropout(0.5))
 model.add(Dense(units=10))
 model.add(Dense(units=1))
 print("Training images: {0}".format(len(train_image_samples)))
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 history_object = model.fit_generator(train_generator, steps_per_epoch=len(train_image_samples) / BATCH_SIZE,
                                      validation_data=validation_generator,
-                                     validation_steps=len(validation_image_samples) / BATCH_SIZE, epochs=10, verbose=0)
+                                     validation_steps=len(validation_image_samples) / BATCH_SIZE, epochs=5, verbose=1)
 
 model.save('model.h5')
 
