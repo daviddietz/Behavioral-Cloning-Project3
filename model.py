24eade5dbdf32faeca839e22fd5b0deb8ebb810c
daviddietz <12344307+dcDun@users.noreply.github.com>
2018-01-02 21:37:33 -0600

    fixed critical bug in image processing

--- model.py
+++ model.py
@@ -2,12 +2,11 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Dropout, MaxPooling2D
+from keras.layers import Flatten, Dense, Lambda, Dropout
 from keras.layers.convolutional import Cropping2D, Convolution2D
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
-from random import randint
 
 sample_images = []
 with open('../Data/driving_log.csv') as csvfile:
@@ -16,7 +15,7 @@ with open('../Data/driving_log.csv') as csvfile:
         sample_images.append(line)
 
 train_image_samples, validation_image_samples = train_test_split(sample_images, test_size=0.2)
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 
 
 def generator(samples, batch_size=BATCH_SIZE):
@@ -32,18 +31,14 @@ def generator(samples, batch_size=BATCH_SIZE):
             for batch_sample in batch_samples:
                 for imageAngle in range(3):
                     measurement = float(batch_sample[3])
-                    # if measurement == 0.00:
-                    #     keep_prob = randint(0, 3)
-                    #     if keep_prob == 1:
-                    #         continue
-                    correction = 0.2
+                    correction = 0.25
                     if imageAngle == 1:
                         # Apply correction to left image
                         measurement = measurement + correction
                     if imageAngle == 2:
                         # Apply correction to right image
                         measurement = measurement - correction
-                    source_path = line[imageAngle]
+                    source_path = batch_sample[imageAngle]
                     filename = source_path.split('/')[-1]
                     current_path = '../Data/IMG/' + filename
                     image = cv2.imread(current_path)
@@ -63,12 +58,11 @@ train_generator = generator(train_image_samples, batch_size=BATCH_SIZE)
 validation_generator = generator(validation_image_samples, batch_size=BATCH_SIZE)
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255 - 0.5, input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 255 - 0.5, input_shape=(160, 320, 3), output_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
-#model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
 model.add(Flatten())
@@ -82,12 +76,12 @@ print("Training images: {0}".format(len(train_image_samples)))
 model.compile(loss='mse', optimizer='adam')
 history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_image_samples),
                                      validation_data=validation_generator,
-                                     nb_val_samples=len(validation_image_samples), nb_epoch=8, verbose=1)
+                                     nb_val_samples=len(validation_image_samples), nb_epoch=5, verbose=1)
 
 model.save('model.h5')
 
 ## print the keys contained in the history object
-print(history_object.history.keys())
+#print(history_object.history.keys())
 
 ## plot the training and validation loss for each epoch
 # plt.plot(history_object.history['loss'])
